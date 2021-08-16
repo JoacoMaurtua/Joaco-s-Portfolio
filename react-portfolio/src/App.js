8bc9cb8b19afff46a3d60e4496ed0fa179ad0b06
JoacoMaurtua <joacomaurtua@gmail.com>
2021-08-16 01:21:48 -0500

    particles.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import SideBar from './components/SideBar';
+import styled from 'styled-components';
+import HomePage from './pages/HomePage';
+
+const MainContentStyled = styled.main`
+  position: relative;
+  margin-left: 16.3rem;
+  min-height: 100vh;
+
+
+`;
 
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+      <MainContentStyled>
+          <HomePage />
+      </MainContentStyled>
     </div>
   );
 }
